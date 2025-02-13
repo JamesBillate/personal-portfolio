9aa8f922bc8b66de745bdd581bb49fb7bc6cbbae
JamesBillate <james.billate@ciit.edu.ph>
2025-02-13 09:07:11 +0800

    Added some animations

--- personal-portfolio/src/main/components/Home.jsx
+++ personal-portfolio/src/main/components/Home.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { motion } from "motion/react";
+
 import Projects from "./Projects";
 import About from "./About";
 import Contact from "./Contact";
@@ -8,13 +10,34 @@ import Navbar from "./Navbar";
 import "./styles/Home.css";
 
 const skillStyle =
-  "mb-15 font-bold text-8xl bg-gradient-to-r from-emerald-300 via-green-500 to-yellow-300 text-transparent bg-clip-text animate-gradient";
+  "mb-15 pb-2 font-bold text-8xl bg-gradient-to-r from-emerald-300 via-green-500 to-yellow-300 text-transparent bg-clip-text animate-gradient";
+
+const skillTitles = ["Web Developer", "Photographer", "Photo Editor"];
 
 const Home = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % skillTitles.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  });
+
   return (
     <div className="home mb-10">
       <h1 className="mb-1 text-3xl">Hello, I am James Billate a</h1>
-      <p className={skillStyle}>Web Developer</p>
+
+      <motion.p
+        className={skillStyle}
+        key={skillTitles[index]}
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
+        {skillTitles[index]}
+      </motion.p>
 
       <Router>
         <Navbar />

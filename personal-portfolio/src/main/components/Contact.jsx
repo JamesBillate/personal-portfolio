f03ea16878ac165d4d2fb2d72c912cd9853000b7
JamesBillate <james.billate@ciit.edu.ph>
2025-02-14 12:31:03 +0800

    Further enhancements of contact page

--- personal-portfolio/src/main/components/Contact.jsx
+++ personal-portfolio/src/main/components/Contact.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
+import connect_data from "./data/connect_data";
+
 const Contact = () => {
+  const [social, setSocial] = useState(
+    "If you are interested to work with me, get in touch and lets connect."
+  );
+
+  function currentSocial(getSocial) {
+    setSocial(getSocial);
+  }
+
   return (
-    <div className="w-screen pb-15">
+    <div className="w-screen">
       <Header pageName="Connect" />
       <div className="mx-15 grid grid-cols-1 md:grid-cols-2">
-        <div className="order-2 md:order-1">Socials area</div>
+        <div className="py-5 grid grid-cols-5 gap-x-2 gap-y-5 order-2 items-center md:order-1">
+          {connect_data.map((connect) => (
+            <a
+              onMouseEnter={() => currentSocial(connect.hover)}
+              onMouseLeave={() =>
+                currentSocial(
+                  "If you are interested to work with me, get in touch and lets connect."
+                )
+              }
+              key={connect.key}
+              href={connect.link}
+              className="cursor-pointer p-5 text-4xl border border-gray-100 rounded-lg text-center"
+            >
+              <i className={connect.icon + " w-10"} />
+            </a>
+          ))}
+        </div>
         <div className="text-right order-1 md:order-2 ml-30">
           <h1 className="text-4xl mb-4">
             Let's create innovative ideas and make it to life!
           </h1>
-          <p className="text-xl mb-7">
-            If you are interested to work with me, get in touch and lets
-            connect.
-          </p>
+          <p className="text-xl mb-7">{social}</p>
         </div>
       </div>
     </div>
---
+++ personal-portfolio/src/main/components/data/connect_data.jsx
@@ -0,0 +1,33 @@
+const connect_data = [
+  {
+    key: 1,
+    icon: "fa-regular fa-envelope",
+    link: "james.billate@ciit.edu.ph",
+    hover: "Email if you want business.",
+  },
+  {
+    key: 2,
+    icon: "fa-brands fa-facebook-f",
+    link: "https://www.facebook.com/james.billate/",
+    hover: "Facebook if something personal.",
+  },
+  {
+    key: 3,
+    icon: "fa-brands fa-instagram",
+    link: "https://www.instagram.com/_zicronphotos/",
+    hover: "Instagram portfolio of my photographs.",
+  },
+  {
+    key: 4,
+    icon: "fa-brands fa-linkedin-in",
+    link: "https://www.instagram.com/_zicronphotos/",
+    hover: "LinkendIn for professional talks.",
+  },
+  {
+    key: 5,
+    icon: "fa-brands fa-github",
+    link: "https://github.com/JamesBillate",
+    hover: "GitHub repository for codes.",
+  },
+];
+export default connect_data;

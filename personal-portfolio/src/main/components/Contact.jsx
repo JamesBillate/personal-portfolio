537a468c1dbaf473bf9527aa2f8ba19cc7a5b52a
JamesBillate <james.billate@ciit.edu.ph>
2025-02-14 11:31:11 +0800

    Updated Contacts

--- personal-portfolio/src/main/components/Contact.jsx
+++ personal-portfolio/src/main/components/Contact.jsx
@@ -5,9 +5,17 @@ const Contact = () => {
   return (
     <div className="w-screen pb-15">
       <Header pageName="Connect" />
-      <div className="mx-15 flex flex-wrap gap-5">
-        <h2>Contact Me</h2>
-        <p>Email: your.email@example.com</p>
+      <div className="mx-15 grid grid-cols-1 md:grid-cols-2">
+        <div className="order-2 md:order-1">Socials area</div>
+        <div className="text-right order-1 md:order-2 ml-30">
+          <h1 className="text-4xl mb-4">
+            Let's create innovative ideas and make it to life!
+          </h1>
+          <p className="text-xl mb-7">
+            If you are interested to work with me, get in touch and lets
+            connect.
+          </p>
+        </div>
       </div>
     </div>
   );

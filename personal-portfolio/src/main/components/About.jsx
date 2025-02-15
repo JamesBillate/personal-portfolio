a136f41f31bf016f0bc64f060c29247ea48044de
JamesBillate <james.billate@ciit.edu.ph>
2025-02-15 20:04:27 +0800

    some information in about page

--- personal-portfolio/src/main/components/About.jsx
+++ personal-portfolio/src/main/components/About.jsx
@@ -6,8 +6,21 @@ const About = () => {
     <div className="w-screen pb-15">
       <Header pageName="AboutMe" />
       <div className="mx-15 flex flex-wrap gap-5">
-        <h2>About Me</h2>
-        <p>This section contains information about me.</p>
+        <h2>Hi! I am James Derick Billate</h2>
+        <p>
+          a student programmer from CIIT College of Arts and Technology under
+          Senior High School track of Programming. I am an 18-year-old Filipino
+          who is currently located in Quezon City. I started to became curious
+          about programming when our teacher from grade 8 introduces us to basic
+          HTML documents. From that point on it became more interesting and
+          really pursued learning the field. As a centennial who uses technology
+          at its highs and lows, I want to become a part of the change to make
+          the society advance in current technology that will soon help lives
+          most especially my kababayans the Filipinos.
+        </p>
+
+        <h2>Technical Skills</h2>
+        <p>Web Devleopenf</p>
       </div>
     </div>
   );
